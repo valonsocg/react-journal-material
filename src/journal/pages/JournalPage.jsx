9466164a72eae6ce9ab6c12c1d7c09e34ca07e87
valonsocg <valonsocg@gmail.com>
2025-05-17 17:31:53 -0500

    activar nota creada

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,33 +1,40 @@
-import { IconButton } from '@mui/material';
-import { AddOutlined } from '@mui/icons-material';
+import { useDispatch } from "react-redux";
 
-import { JournalLayout } from '../layout/JournalLayout';
-import { NoteView, NothingSelectedView } from '../views';
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+
+import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
+import { startNewNote } from "../../store/journal/thunks";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
-      
       {/* <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography> */}
 
       <NothingSelectedView />
       {/* <NoteView /> */}
 
-
       <IconButton
-        size='large'
+        onClick={onClickNewNote}
+        size="large"
         sx={{
-          color: 'white',
-          backgroundColor: 'error.main',
-          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
-          position: 'fixed',
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
           right: 50,
-          bottom: 50
+          bottom: 50,
         }}
       >
         <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
-
     </JournalLayout>
-  )
-}
+  );
+};
---
+++ src/store/journal/thunks.js
@@ -0,0 +1,26 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
+
+export const startNewNote = () => {
+  return async (dispatch, getState) => {
+    //uid
+
+    const { uid } = getState().auth;
+
+    const newNote = {
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+    };
+
+    const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+
+    await setDoc(newDoc, newNote);
+
+    newNote.id = newDoc.id;
+
+    //dispatch
+    //dispatch(newNote)
+    //dispatch(activeNote)
+  };
+};
